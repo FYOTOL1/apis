f1bfa2eb8e75d62bdbfc4b844d9e9d0cc838fa61
EGY-Temble <fyotol@gmail.com>
2023-11-05 14:07:40 +0300

    a

--- 3ayez/connect.php
+++ 3ayez/connect.php
@@ -6,7 +6,7 @@ try {
     if ($dev === true) {
         $GLOBALS["db"] = new PDO("mysql:host=localhost;dbname=apis", "root", "");
     } else {
-        $GLOBALS["db"] = new PDO("mysql:host=premium-host.dnsco.in;dbname=if0_35242478_app", "doplario_ahmed", "fyotol_2008");
+        $GLOBALS["db"] = new PDO("mysql:host=premium-host.dnsco.in;dbname=doplario_ahmed", "doplario_ahmed", "fyotol_2008");
     }
 } catch (PDOException $th) {
     echo "Field Connect: " . $th;
