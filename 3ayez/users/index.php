1827e2247a8ba1ff2696e032dd40a79d37bec77b
EGY-Temble <fyotol@gmail.com>
2023-11-07 18:45:07 +0300

    full rest api

--- 3ayez/users/index.php
+++ 3ayez/users/index.php
@@ -36,6 +36,7 @@ class Requests
                     $cmd->bindParam(':email', $d["email"]);
                     $cmd->bindParam(':password', $d["password"]);
                     $cmd->execute();
+                    http_response_code(201);
                     echo json_encode(["message" => "تم إدراج المستخدم بنجاح"]);
                 } else {
                     echo json_encode(["message" => "No Data Found", "data" => $d]);
@@ -48,11 +49,44 @@ class Requests
 
     public function PATCH()
     {
-        try {
-            $jd = file_get_contents("php://input");
-            $d = json_encode($jd, true);
-            echo $d;
-        } catch (Exception $err) {
+        if ($_SERVER["REQUEST_METHOD"] === "PATCH") {
+            try {
+                $json_data = file_get_contents("php://input");
+                $d = json_decode($json_data, true);
+                if (isset($d["id"]) && isset($d["username"]) && isset($d["email"]) && isset($d["password"])) {
+                    $id = $d["id"];
+                    foreach ($d as $k => $e) {
+                        $column = $k;
+                        $value = strtolower($e);
+                        $cmd = $GLOBALS["db"]->prepare("UPDATE `users` SET $column = :val WHERE `users`.`id` = :id");
+                        $cmd->bindParam(":id", $id);
+                        $cmd->bindParam(":val", $value);
+                        $cmd->execute();
+                    }
+                    // RESULT
+                    $cmd = $GLOBALS["db"]->prepare("SELECT `id`, `username`, `email`, `password` FROM `users` WHERE id = :id;");
+                    $cmd->bindParam(":id", $id);
+                    $cmd->execute();
+                    echo json_encode($cmd->fetch(PDO::FETCH_ASSOC));
+                }
+            } catch (Exception $err) {
+                echo json_encode(["msg" => $err]);
+            }
+        }
+    }
+    public function DELETE()
+    {
+        if ($_SERVER["REQUEST_METHOD"] === "DELETE" && isset($_REQUEST["id"])) {
+            try {
+                $cmd = $GLOBALS["db"]->prepare("DELETE FROM `users` WHERE id = :id");
+                $cmd->bindParam(":id", $_REQUEST["id"]);
+                $cmd->execute();
+                http_response_code(200);
+                echo json_encode(["msg" => "DELETED SUCCESSFULLY", "id" => $_REQUEST["id"]]);
+            } catch (EXCEPTION $err) {
+                http_response_code(404);
+                echo json_encode(["msg" => $err]);
+            }
         }
     }
 }
@@ -62,3 +96,7 @@ $req = new Requests();
 echo $req->GET();
 
 echo $req->POST();
+
+echo $req->PATCH();
+
+echo $req->DELETE();
